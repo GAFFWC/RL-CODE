16c901e6354f6d84111183fa593716b839b2b16f
GAFFWC <41960405+GAFFWC@users.noreply.github.com>
2018-08-19 18:19:28 +0900

    8x8 Frozenlake Reinforcement Learning by Deep-SARSA

--- myRL/frozenlake(Deep-SARSA).py
+++ myRL/frozenlake(Deep-SARSA).py
@@ -29,9 +29,9 @@ env = gym.make('FrozenLake-v3')        # is_slippery False
 class DeepSARSAgent:
     def __init__(self):
         self.load_model = False
-        self.action_space = [0, 1, 2, 3, 4]
+        self.action_space = [0, 1, 2, 3]
         self.action_size = len(self.action_space)
-        self.state_size = 15
+        self.state_size = 26
         self.discount_factor = 0.99
         self.learning_rate = 0.001
 
@@ -39,9 +39,7 @@ class DeepSARSAgent:
         self.epsilon_decay = 0.9999
         self.epsilon_min = 0.01
         self.model = self.build_model()
-        
-        if self.load_model:
-            self.model.load_weights('./deep_sarsa_trained.h5')
+        self.model.load_weights('./deep_sarsa_trained.h5')
 
     def build_model(self):
         model = Sequential()
@@ -56,7 +54,8 @@ class DeepSARSAgent:
             return random.randrange(self.action_size)
         else:
             state = np.float32(state)
-            q_values = self.model.predic(state)
+            state = np.reshape(state, [1, 26])			
+            q_values = self.model.predict(state)
             return np.argmax(q_values[0])
 
     def train_model(self, state, action, reward, next_state, next_action, done):
@@ -65,19 +64,22 @@ class DeepSARSAgent:
 
 
         state = np.float32(state)
+        state = np.reshape(state, [1, 26])
         next_state = np.float32(next_state)
         target = self.model.predict(state)[0]
+        next_state = np.reshape(next_state, [1, 26])
+        
 
         if done:
             target[action] = reward
+            #print(target)
         else:
             target[action] = (reward + self.discount_factor * self.model.predict(next_state)[0][next_action])
 
-        target = np.reshape(target, [1, 5])
+        target = np.reshape(target, [1, 4])
         self.model.fit(state, target, epochs = 1, verbose = 0)
+		
 
-env.render()    
-state = [0, 0]
 cnt = 1
 path = []
 
@@ -93,36 +95,88 @@ arrow_keys = {
     '\x1b[D' : LEFT
 }
 
+reward = [[0.00] * 8 for _ in range(8)]
+
+reward[6][1] = -1.0
+reward[6][4] = -1.0
+reward[6][6] = -1.0
+reward[7][3] = -1.0
+reward[5][2] = -1.0
+reward[5][6] = -1.0
+reward[5][1] = -1.0
+reward[4][3] = -1.0
+reward[2][3] = -1.0
+reward[3][5] = -1.0
+reward[3][3] = -1.0
+reward[7][7] = 1.0
+    
+def state_after_action(state, action_index):
+    row = state[0]
+    col = state[1]
+    if arrow_keys[action_index] == 0: #LEFT
+        col = max(col - 1, 0)
+    elif arrow_keys[action_index] == 1: # down
+        row = min(row+1, 7)
+    elif arrow_keys[action_index] == 2: # right
+        col = min(col + 1, 7)
+    elif arrow_keys[action_index] == 3: # up
+        row = max(row - 1, 0)
+    return int(row), int(col)
 
 if __name__ == "__main__":
     agent = DeepSARSAgent()
     global_step = 0
+    temp = [0, 0]
     scores, episodes = [], []
-    for e in range(EPISODES):
-        done = False
+    for e in range(2500):
+        d = False
         score = 0
-        state = [0, 0]
-        state = np.reshape(state, [1, 15])
+        state = [0, 0, 2, 3, 3, 3, 3, 5, 4, 3, 5, 1, 5, 2, 5, 6, 6, 1, 6, 4, 6, 6, 7, 3, 7, 7]
+       
+        #print(state)
 
-        while not done:
+        while True:
             global_step += 1
+            next_state =  [0, 0, 2, 3, 3, 3, 3, 5, 4, 3, 5, 1, 5, 2, 5, 6, 6, 1, 6, 4, 6, 6, 7, 3, 7, 7]
             action = agent.get_action(state)
-            next_state, reward, done, info = env.step(action)
-            next_state = np.reshape(next_state, [1, 15])
+            #print(action)
+            n, r, d, info = env.step(action)
+            #print(n)
+            #print(n, r, d, info)
+            if action == 0:
+                next_state[0], next_state[1] = state_after_action(state, '\x1b[D')
+            elif action == 1:
+                next_state[0], next_state[1]  = state_after_action(state, '\x1b[B')          
+            elif action == 2:
+                next_state[0], next_state[1]  = state_after_action(state, '\x1b[C')           
+            else:
+                next_state[0], next_state[1]  = state_after_action(state,  '\x1b[A')
+            
+            print(next_state)
+            state = np.reshape(state, [1, 26])
+            rew = reward[next_state[0]][next_state[1]]
             next_action = agent.get_action(next_state)
-            agent.train_model(state, action, reward, next_state, next_action, done)
+            agent.train_model(state, action, rew, next_state, next_action, d)
             state = next_state
-            score += reward
+            #print(state)
+            score += rew
             state = copy.deepcopy(next_state)
+            #print(state)
+            os.system('cls')
+            env.render()
+            if d:
 
-            if done:
+                #print(state)
+                #env.render()
+                #print(target)
                 scores.append(score)
                 episodes.append(e)
                 pylab.plot(episodes, scores, 'b')
                 pylab.savefig("./deep-sarsa.png")
+                #print(state)
                 print("episode:", e, " score:", score, "global_step", global_step, " epsilon:", agent.epsilon)
-
-        if e%100 == 0:
+                env.reset()
+                break
             agent.model.save_weights("./deep_sarsa.h5")
            
         
